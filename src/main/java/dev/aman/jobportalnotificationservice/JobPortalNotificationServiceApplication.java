5da6b7c2ee49b00c15e10755ccdf9f144cfcf296
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-23 20:26:01 +0530

    Created Notification Service

---
+++ src/main/java/dev/aman/jobportalnotificationservice/JobPortalNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aman/jobportalnotificationservice/JobPortalNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportalnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
